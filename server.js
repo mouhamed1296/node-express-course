54313f01b70b90e0ec457caab692ec33e307f18c
mbayelel@gmail.com <mbayelel@gmail.com>
2021-10-26 18:36:06 +0200

    Sending a post request to /login using Postman

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-app.use(bodyParser.json());
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
 
 const mockUserData=[
     {name:'Mark'},
